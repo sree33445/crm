e728ec064f8b9368d8651be837f042d31524618d
MsJoitson <msjoitson@gmail.com>
2024-07-02 10:49:46 +0530

    css

--- client/app/components/Login.jsx
+++ client/app/components/Login.jsx
@@ -1,22 +1,84 @@
-'use client';
-import { Button } from '@nextui-org/button'
-import { Input } from '@nextui-org/input'
-import Link from 'next/link';
-import React from 'react'
+"use client";
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import Link from "next/link";
+import React from "react";
+import Head from "next/head";
 
-export default function Login(){
+export default function Login() {
   return (
-    <div className="flex w-full justify-center flex-wrap md:flex-nowrap gap-4 p-12">
-        <form action="submit">
-          <Input isRequired type="text" label='Name' placeholder='Enter your name' />
-          <br />
-          <Input isRequired type="email" label='Email' placeholder='Enter your email' />
-          <br />
-          <Input isRequired type="password" label='Password' placeholder='Enter your password' />
-          <br />
-          <Button as={Link} color='primary' href='/loginsuccess'>Login</Button>
-        </form>
-    </div>
-  )
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <div className="bg-gradient-to-r from-[#F28383] from-10% via-[#9D6CD2] via-30% to-[#481EDC] to-90% flex items-center justify-center h-screen">
+        <div className="max-w-[960px] bg-black-dark grid grid-cols-2 items-center gap-20 p-5 rounded-2xl">
+          <div>
+            <img src="/assets/1.svg" alt="" />
+            <img src="/assets/2.svg" alt="" className="absolute top-36" />
+          </div>
+
+          <div className="max-w-80 grid gap-5">
+            <h1 className="text-5xl font-bold text-white">Login</h1>
+            <p className="text-dull-white">
+              Access to 300+ hours of courses, tutorials, and livestreams
+            </p>
+            <form action="" className="space-y-6 text-white">
+              <div className="relative">
+                <div className="absolute top-1 left-1 bg-white-medium rounded-full p-2 flex items-center justify-center text-blue-300">
+                  <i className="fa-solid fa-envelope-open"></i>
+                </div>
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="w-80 bg-white-light py-2 px-12 rounded-full focus:bg-black-dark focus:outline-none focus:ring-1 focus:ring-neon-blue focus:drop-shadow-lg"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute top-1 left-1 bg-white-medium rounded-full p-2 flex items-center justify-center text-blue-300">
+                  <i className="fa-solid fa-lock"></i>
+                </div>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-80 bg-white-light py-2 px-12 rounded-full focus:bg-black-dark focus:outline-none focus:ring-1 focus:ring-neon-blue focus:drop-shadow-lg"
+                />
+              </div>
+              <button className="bg-gradient-to-r from-blue-400 to-cyan-200 w-80 font-semibold rounded-full py-2">
+                Sign in
+              </button>
+            </form>
+            <div className="text-dull-white border-t border-white-light pt-4 space-y-4 text-sm">
+              <p>
+                Don't have an account?{" "}
+                <a className="text-neon-blue font-semibold cursor-pointer">
+                  Sign up
+                </a>
+              </p>
+              <p>
+                Forgot password?{" "}
+                <a className="text-neon-blue font-semibold cursor-pointer">
+                  Reset password
+                </a>
+              </p>
+              <p>
+                Don't have a password yet?{" "}
+                <a className="text-neon-blue font-semibold cursor-pointer">
+                  Set password
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
 
