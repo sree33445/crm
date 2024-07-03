d9cfe5ba9b2c783470da343776a08db7bd799b38
Sreeraj <sreerajs00000@gmail.com>
2024-07-03 22:47:31 +0530

    css added

--- client/app/components/Login.jsx
+++ client/app/components/Login.jsx
@@ -16,8 +16,11 @@ export default function Login(){
   }, [value]);
 
   return (
-    <div className="flex w-full justify-center flex-wrap md:flex-nowrap gap-4 p-12">
-        <form action="submit">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-blue-400">
+      <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        <div className="mb-4">
+        <form action="submit" className='w-full'>
+          <h1 className="text-2xl font-bold mb-4 color-success">Login Page</h1>
           <Input isRequired type="text" label='Name' placeholder='Enter your name' />
           <br />
           <Input isRequired type="email" label='Email' placeholder='Enter your email' value={value}  isInvalid={isInvalid} color={isInvalid ? 'danger':'default'} errorMessage='Please enter a valid email' onValueChange={setValue} className='max-w-xs' />
@@ -26,6 +29,8 @@ export default function Login(){
           <br />
           <Button as={Link} color='primary' href='/loginsuccess'>Login</Button>
         </form>
+        </div>
+        </div>
     </div>
   )
 }
--- client/app/page.jsx
+++ client/app/page.jsx
@@ -4,6 +4,11 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
+   <div>
     <NavBar />
+    
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-blue-400">
+      </div>
+      </div>
   );
 }
