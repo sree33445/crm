c8887b823e90cdc4f28a3a8369abe5460e31a11a
AlwinAnto41 <alwinanto41@gmail.com>
2024-08-08 15:48:16 +0530

    update navbar

--- client/app/components/NavBar.jsx
+++ client/app/components/NavBar.jsx
@@ -7,10 +7,6 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 import { IoIosNotifications } from "react-icons/io";
 
 const NavBar = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [expanded, setExpanded] = useState(true);
   const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const toggleDropdown = () => {
@@ -18,36 +14,31 @@ const NavBar = () => {
   };
 
   return (
-    <div className="bg-gray-800 flex flex-wrap justify-between items-center py-4 px-4 relative">
+    <div className="bg-gray-800 flex justify-between items-center py-4 px-6 relative">
       <div className="flex items-center space-x-3">
         <Image src="/image/crmLogo.png" alt="CRM Logo" width={48} height={48} />
         <h1 className="font-bold text-white">CRM</h1>
       </div>
 
-      <div className="ml-auto flex items-center space-x-4">
-        <div className="flex p-2 relative items-center bg-neutral-200 rounded-2xl ">
-          <IoPersonCircleOutline
-            color="black"
-            className="w-8 h-9 rounded-md mr-2"
-          />
-
-          <div className="leading-4">
-            <h2 className="text-s text-black font-semibold">John Doe</h2>
+      <div className="flex items-center space-x-4">
+        <div className="flex items-center bg-neutral-200 rounded-full py-1 px-3 space-x-2">
+          <IoPersonCircleOutline color="black" className="w-8 h-8" />
+          <div className="leading-tight">
+            <h2 className="text-sm text-black font-semibold">John Doe</h2>
             <span className="text-xs text-black">johndoe@gmail.com</span>
           </div>
         </div>
         
-        <IoIosNotifications color="white" className="cursor-pointer h-8 w-7" />
-
+        <IoIosNotifications color="white" className="cursor-pointer h-8 w-8" />
         <SlOptionsVertical
           color="white"
           onClick={toggleDropdown}
-          className="cursor-pointer"
+          className="cursor-pointer h-6 w-6"
         />
       </div>
 
       {dropdownVisible && (
-        <div className="absolute right-0 top-full mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-10">
+        <div className="absolute right-6 top-14 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-10">
           <ul className="list-none p-0 m-0">
             <li className="border-b border-gray-300">
               <Link
